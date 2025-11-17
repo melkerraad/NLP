9dc9784623338550eb0012858f6ad0f3118f89c1
Melker Rååd <melkerr@chalmers.se>
2025-11-17 19:22:50 +0100

    made changes for big run

--- Ass2/rbt.sh
+++ Ass2/rbt.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --gres=gpu:L4:1
+#SBATCH --gres=gpu:L40s:1
 #SBATCH -c 8
 #SBATCH --mem=64G
-#SBATCH -o big_output/big_train.out
-#SBATCH -e big_output/big_train.err
-#SBATCH -p short
+#SBATCH -o output/big_train.out
+#SBATCH -e output/big_train.err
+#SBATCH -p long
 
 cd /data/users/melkerr/NLP
 python3 -m Ass2.run_big_train
