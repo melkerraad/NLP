db0637610202bb5fec5a98d03535a6da37fedc7e
melkerraad <melkerr.raad@yahoo.se>
2025-12-01 15:11:58 +0100

    initial steps

---
+++ Ass5/ld.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --gres=gpu:L4:3
+#SBATCH -c 16
+#SBATCH --mem=256G
+#SBATCH -o resultsd.out
+#SBATCH -e resultsd.err
+
+python3 load_datasets.py
---
+++ Ass5/load_datasets.py
@@ -0,0 +1,12 @@
+import pandas as pd
+tmp_data = pd.read_json("ori_pqal.json").T
+# some labels have been defined as "maybe", only keep the yes/no answers
+tmp_data = tmp_data[tmp_data.final_decision.isin(["yes", "no"])]
+
+documents = pd.DataFrame({"abstract": tmp_data.apply(lambda row: (" ").join(row.CONTEXTS+[row.LONG_ANSWER]), axis=1),
+             "year": tmp_data.YEAR})
+questions = pd.DataFrame({"question": tmp_data.QUESTION,
+             "year": tmp_data.YEAR,
+             "gold_label": tmp_data.final_decision,
+             "gold_context": tmp_data.LONG_ANSWER,
+             "gold_document_id": documents.index})
